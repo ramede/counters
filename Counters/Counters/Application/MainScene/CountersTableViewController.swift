86a1c4368e6005cdc65b1db51033603a0e6e618f
Râmede Bento <ramede@gmail.com>
2021-10-09 14:51:04 -0300

    Adjusting header table view in main scene and layouting counter table view cell

--- Counters/Counters/Application/MainScene/CountersTableViewController.swift
+++ Counters/Counters/Application/MainScene/CountersTableViewController.swift
@@ -8,19 +8,60 @@
 import UIKit
 
 class CountersTableViewController: UITableViewController {
+    
 
-    private var coutersSearchBar: UISearchBar = {
+    private var searchBarContainer = UIView()
+    
+    private var countersSearchBar: UISearchBar = {
         let searchBar = UISearchBar()
         searchBar.searchBarStyle = UISearchBar.Style.minimal
         searchBar.placeholder = "Search"
         searchBar.sizeToFit()
         searchBar.isTranslucent = false
-        searchBar.backgroundImage = UIImage()
+        searchBar.backgroundColor = .systemGray6
         searchBar.translatesAutoresizingMaskIntoConstraints = false
         return searchBar
     }()
-        
-    var dataSource: [String] = ["UM","DOIS","TRES","QUATRO"]
+    
+    private let flexibleSpace: UIBarButtonItem = {
+        let buttonItem = UIBarButtonItem(
+            barButtonSystemItem: .flexibleSpace,
+            target: nil,
+            action: nil
+        )
+        return buttonItem
+    }()
+    
+    private var editButton: UIBarButtonItem = {
+        let buttonItem = UIBarButtonItem(
+            title: "Edit",
+            style: .plain,
+            target: nil,
+            action: nil
+        )
+        return buttonItem
+    }()
+    
+    private let counterSumDisplayer: UIBarButtonItem = {
+        let buttonItem = UIBarButtonItem(
+            title: "",
+            style: .plain,
+            target: self,
+            action: nil
+        )
+        return buttonItem
+    }()
+    
+    private let addButton: UIBarButtonItem = {
+        let buttonItem = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addTapped)
+        )
+        return buttonItem
+    }()
+    
+    var dataSource: [String] = ["Apple eaten","Number of times I’ve forgotten my mother’s name because I was high on Frugelés.","Cups of coffee","Records played"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,52 +74,45 @@ class CountersTableViewController: UITableViewController {
     
     private func setupToolbar() {
         navigationController?.setToolbarHidden(false, animated: true)
-        
-        let spaceItem = UIBarButtonItem(
-            barButtonSystemItem: .flexibleSpace,
-            target: nil,
-            action: nil
-        )
-        
-        let statusItem = UIBarButtonItem(
-            title: "4 items · Counted 16 times",
-            style: .plain,
-            target: self,
-            action: nil
-        )
-        statusItem.isEnabled = false
-                
-        let addItem = UIBarButtonItem(
-            barButtonSystemItem: .add,
-            target: self,
-            action: #selector(addTapped)
-        )
-        
-        setToolbarItems([spaceItem, statusItem, spaceItem, addItem], animated: true)
+        setToolbarItems([flexibleSpace, counterSumDisplayer, flexibleSpace, addButton], animated: true)
     }
     
     private func setupNavigationBar() {
         navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.backgroundColor = .systemGray6
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.setStatusBar(backgroundColor: .systemGray6)
         navigationItem.title = "Couters"
-        let backButton = UIBarButtonItem(title: "Edit", style: .plain, target: nil, action: nil);
-        navigationItem.leftBarButtonItem = backButton
+        navigationItem.leftBarButtonItem = editButton
     }
     
     private func setupTableView() {
         tableView.dataSource = self
         tableView.delegate = self
-        tableView.tableHeaderView = coutersSearchBar
+        tableView.tableHeaderView = countersSearchBar
+        tableView.backgroundColor = .systemGray5
         tableView.keyboardDismissMode = .onDrag
+        tableView.separatorStyle = .none
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 180
         tableView.register(CountersTableViewCell.self, forCellReuseIdentifier: "cell")
     }
-
+    
     private func setupSearchBar() {
-        coutersSearchBar.delegate = self
+        countersSearchBar.delegate = self
     }
     
     private func setupConstraints() {
-        coutersSearchBar.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8).isActive = true
-        coutersSearchBar.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8).isActive = true
+        countersSearchBar.leadingAnchor.constraint(
+            equalTo: view.safeAreaLayoutGuide.leadingAnchor,
+            constant: 0
+        ).isActive = true
+
+        countersSearchBar.trailingAnchor.constraint(
+            equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+            constant: 0
+        ).isActive = true
     }
     
     @objc
@@ -104,11 +138,27 @@ extension CountersTableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(
-                withIdentifier: "cell",
-                for: indexPath
+            withIdentifier: "cell",
+            for: indexPath
         ) as? CountersTableViewCell else { return UITableViewCell() }
+        cell.selectionStyle = .none
+        
+        cell.counterDescription = dataSource[indexPath.row]
         return cell
     }
 }
 
 
+extension UINavigationController {
+    func setStatusBar(backgroundColor: UIColor) {
+        let statusBarFrame: CGRect
+        if #available(iOS 13.0, *) {
+            statusBarFrame = view.window?.windowScene?.statusBarManager?.statusBarFrame ?? CGRect.zero
+        } else {
+            statusBarFrame = UIApplication.shared.statusBarFrame
+        }
+        let statusBarView = UIView(frame: statusBarFrame)
+        statusBarView.backgroundColor = backgroundColor
+        view.addSubview(statusBarView)
+    }
+}
---
+++ Counters/Counters/Application/MainScene/Views/CountersTableViewCell.swift
@@ -0,0 +1,178 @@
+//
+//  CountersTableViewCell.swift
+//  Counters
+//
+//  Created by Râmede on 06/10/21.
+//
+
+import UIKit
+
+
+// MARK: - Constants
+
+
+class CountersTableViewCell: UITableViewCell {
+    
+    // MARK: - Properties
+    private var mainView = UIView()
+    private var counterLabel = UILabel()
+    private var dividerView = UIView()
+    private var descriptionLabel = UILabel()
+    private var counterStepper = UIStepper()
+    
+    // MARK: - Configuration
+    var counterDescription: String = "" {
+        didSet {
+            descriptionLabel.text = counterDescription
+        }
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        backgroundColor = .clear
+        setup()
+    }
+    
+    @objc
+    private func dismissAction() {
+    }
+    
+    @objc
+    private func saveImageAction() {
+    }
+    
+}
+
+// MARK: - Private Constants
+
+private extension CountersTableViewCell {
+    enum Constants {
+        enum MainView {
+            static let top: CGFloat = 16
+            static let trailing: CGFloat = -16
+            static let leading: CGFloat = 16
+            static let radius: CGFloat = 8
+        }
+
+        enum CounterLabel {
+            static let top: CGFloat = 16
+            static let trailing: CGFloat = -10
+            static let leading: CGFloat = 16
+            static let width: CGFloat = 35
+        }
+        
+        enum DescriptionLabel {
+            static let top: CGFloat = 16
+            static let trailing: CGFloat = -14
+            static let bottom: CGFloat = -9
+        }
+        
+        enum CounterStepper {
+            static let tailing: CGFloat = -14
+            static let bottom: CGFloat = -14
+        }
+        
+        enum DividerView {
+            static let width: CGFloat = 2
+        }
+    }
+    
+    enum Font {
+        enum Counter {
+            static let size : CGFloat = 22
+            static let bold = UIFont.systemFont(ofSize: 22, weight: .bold)
+        }
+        
+        enum Description {
+            static let regular = UIFont.systemFont(ofSize: 17, weight: .regular)
+        }
+    }
+}
+
+// MARK: - Private Implementation
+private extension CountersTableViewCell {
+    func setup() {
+        setupMainView()
+        setupCounterLabel()
+        setupDividerView()
+        setupDescriptionLabel()
+        setupCounterStepper()
+        setupHierarchy()
+        setupConstraints()
+    }
+
+    func setupMainView() {
+        mainView.translatesAutoresizingMaskIntoConstraints = false
+        mainView.layer.cornerRadius = Constants.MainView.radius
+        mainView.backgroundColor = .white
+    }
+    
+    func setupCounterLabel() {
+        var font: UIFont = Font.Counter.bold
+        if let descriptor = Font.Counter.bold.fontDescriptor.withDesign(.rounded) {
+            font = UIFont(descriptor: descriptor, size: Font.Counter.size)
+        }
+        
+        counterLabel.widthAnchor.constraint(equalToConstant: Constants.CounterLabel.width).isActive = true
+        counterLabel.font = font
+        counterLabel.translatesAutoresizingMaskIntoConstraints = false
+        counterLabel.numberOfLines = 0
+        counterLabel.textColor = UIColor(named: "AccentColor")
+        counterLabel.text = "99" // TODO: Binding
+        counterLabel.textAlignment = .right
+    }
+    
+    func setupDividerView() {
+        dividerView.translatesAutoresizingMaskIntoConstraints = false
+        dividerView.widthAnchor.constraint(equalToConstant: Constants.DividerView.width).isActive = true
+        dividerView.backgroundColor = .systemGray5
+    }
+    
+    func setupDescriptionLabel() {
+        descriptionLabel.font = UIFontMetrics(forTextStyle: .body).scaledFont(for: Font.Description.regular)
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.lineBreakMode = .byWordWrapping
+        descriptionLabel.textColor = UIColor(named: "DescriptionText")
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func setupCounterStepper() {
+        counterStepper.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func setupHierarchy() {
+        contentView.addSubview(mainView)
+        mainView.addSubview(counterLabel)
+        mainView.addSubview(dividerView)
+        mainView.addSubview(descriptionLabel)
+        mainView.addSubview(counterStepper)
+    }
+
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            mainView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: Constants.MainView.top),
+            mainView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: Constants.MainView.trailing),
+            mainView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            mainView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: Constants.MainView.leading),
+            
+            counterLabel.topAnchor.constraint(equalTo: mainView.topAnchor, constant: Constants.CounterLabel.top),
+            counterLabel.trailingAnchor.constraint(equalTo: dividerView.trailingAnchor, constant: Constants.CounterLabel.trailing),
+            counterLabel.leadingAnchor.constraint(equalTo: mainView.leadingAnchor, constant: Constants.CounterLabel.leading),
+            
+            dividerView.topAnchor.constraint(equalTo: mainView.topAnchor),
+            dividerView.trailingAnchor.constraint(equalTo: descriptionLabel.leadingAnchor, constant: Constants.CounterLabel.trailing),
+            dividerView.bottomAnchor.constraint(equalTo: mainView.bottomAnchor),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: mainView.topAnchor, constant: Constants.DescriptionLabel.top),
+            descriptionLabel.trailingAnchor.constraint(equalTo: mainView.trailingAnchor, constant: Constants.DescriptionLabel.trailing),
+            descriptionLabel.bottomAnchor.constraint(equalTo: counterStepper.topAnchor, constant: Constants.DescriptionLabel.bottom),
+            
+            counterStepper.trailingAnchor.constraint(equalTo: mainView.trailingAnchor, constant: Constants.CounterStepper.tailing),
+            counterStepper.bottomAnchor.constraint(equalTo: mainView.bottomAnchor, constant: Constants.CounterStepper.bottom)
+        ])
+    }
+}
